c6e1053571f1ab7fb8072a50e223f4e96eaef7f2
tsdmCode <116952397+tsdmCode@users.noreply.github.com>
2025-08-27 09:17:46 +0200

    Done

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,52 +1,44 @@
+//Opgave 1 kode
+function saveName() {
+  const name = document.getElementById('nameInput').value;
+  // din localStorage code her: gem navnet i localStorage
+  localStorage.setItem('name', name);
+  showGreeting();
+}
 
- //Opgave 1 kode
- function saveName() {
-      const name = document.getElementById("nameInput").value;
-         // din localStorage code her: gem navnet i localStorage
+function showGreeting() {
+  // din localStorage code her opret en const med navn savedName som indeholder navnet fra localStorage
+  const savedName = localStorage.getItem('name');
 
-      showGreeting();
-    }
-
-    function showGreeting() {
-           // din localStorage code her opret en const med navn savedName som indeholder navnet fra localStorage
-    
-
-      if (savedName) {
-        document.getElementById("greeting").textContent = `Hej, ${savedName}!`;
-      }
-    }
-
-  
+  if (savedName) {
+    document.getElementById('greeting').textContent = `Hej, ${savedName}!`;
+  }
+}
 
 // Opgave 2 kode
 function saveColor() {
-      const color = document.getElementById("colorPicker").value;
-    // din localStorage code her: gem farven i localStorage
+  const color = document.getElementById('colorPicker').value;
+  // din localStorage code her: gem farven i localStorage
 
+  localStorage.setItem('color', color);
 
-      document.body.style.backgroundColor = color;
-    }
+  document.body.style.backgroundColor = color;
+}
 
+function loadColor() {
+  // din localStorage kode her: opret en const med navn savedColor som indeholder farven fra localStorage
+  const savedColor = localStorage.getItem('color');
 
-    function loadColor() {
-      // din localStorage kode her: opret en const med navn savedColor som indeholder farven fra localStorage
-      
+  if (savedColor) {
+    document.body.style.backgroundColor = savedColor;
+  }
+}
 
-      if (savedColor) {
-        document.body.style.backgroundColor = savedColor;
-      }
-    };
+//--- service functions nix pille! -------------------------------------------------------------------------------------
 
+window.onload = loadApp;
 
-
-    //--- service functions nix pille! -------------------------------------------------------------------------------------
-
-      window.onload = loadApp;
-
-      function loadApp() {
-        showGreeting();
-        loadColor();
-      }
-
-
-
+function loadApp() {
+  showGreeting();
+  loadColor();
+}
